229ea9b86341c2372f46dcf29b6c0e17bd1dcb61
HernanFranco96 <hernan.franco051096@gmail.com>
2019-08-29 07:45:16 -0300

    Pagina web
    -

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Inicio/style.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="Noticias/index.html"><span class="quinto"><img style="width: 25px; height: 25px;" src="Inicio/img/diario.png" alt=""></span>Noticias</a>
+                <li><a href="#"><span class="segundo"><img style="width: 25px; height: 25px;" src="Inicio/img/cup.png" alt=""></span>Eventos</a>
+                    <ul>
+                        <li><a href="Eventos/torneo_1/index.html">Torneo #1</a></li>
+                        <li><a href="Eventos/Disputas/index.html">Disputas</a></li>
+                    </ul>
+                </li>
+                <li><a href="Rangos/index.html"><span class="tercero"><img style="width: 25px; height: 25px;" src="Inicio/img/swords.png" alt=""></span>Rangos</a>
+                    <!--<ul>
+                        <li><a href="#">Rango #1</a></li>
+                        <li><a href="#">Rango #2</a></li>
+                        <li><a href="#">Rango #3</a></li>
+                        <li><a href="#">Rango #4</a></li>
+                        <li><a href="#">Rango #5</a></li>
+                    </ul>-->
+                </li>
+                <li><a href="Reglas/index.html"><span class="cuarto"><img style="width: 25px; height: 25px;" src="Inicio/img/book.png" alt=""></span>Reglas</a></li>
+            </ul>
+        </nav>
+    </header>
+    <footer>
+        <p>Lampone - Hernán Gastón Franco</p>
+        <div class="iconos">
+            <a href=""><img src="Inicio/img/facebook.png"></a>
+            <a href=""><img src="Inicio/img/twitter.png"></a>
+            <a href=""><img src="Inicio/img/instagram.png"></a>
+        </div>
+    </footer>
+</body>
+</html>
